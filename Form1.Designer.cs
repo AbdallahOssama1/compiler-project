e41425b3b905c860c9eab47d7c225b7ddc4da128
BN_Ossama <abdallah.minicooper2017@gmail.com>
2026-03-15 00:54:30 +0200

    initcommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace project_compiler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSplit = new System.Windows.Forms.Button();
+            this.dgvTokens = new System.Windows.Forms.DataGridView();
+            this.lexeme = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Token_Type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTokens)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.BackColor = System.Drawing.Color.White;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.ForeColor = System.Drawing.Color.Black;
+            this.txtInput.Location = new System.Drawing.Point(12, 53);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(598, 366);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnSplit
+            // 
+            this.btnSplit.BackColor = System.Drawing.Color.Lime;
+            this.btnSplit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSplit.Location = new System.Drawing.Point(145, 441);
+            this.btnSplit.Name = "btnSplit";
+            this.btnSplit.Size = new System.Drawing.Size(311, 92);
+            this.btnSplit.TabIndex = 1;
+            this.btnSplit.Text = "Split";
+            this.btnSplit.UseVisualStyleBackColor = false;
+            // 
+            // dgvTokens
+            // 
+            this.dgvTokens.BackgroundColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.dgvTokens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTokens.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.lexeme,
+            this.Token_Type});
+            this.dgvTokens.Location = new System.Drawing.Point(631, 53);
+            this.dgvTokens.Name = "dgvTokens";
+            this.dgvTokens.RowHeadersWidth = 51;
+            this.dgvTokens.RowTemplate.Height = 24;
+            this.dgvTokens.Size = new System.Drawing.Size(397, 260);
+            this.dgvTokens.TabIndex = 2;
+            this.dgvTokens.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // lexeme
+            // 
+            this.lexeme.HeaderText = "lexeme";
+            this.lexeme.MinimumWidth = 6;
+            this.lexeme.Name = "lexeme";
+            this.lexeme.Width = 125;
+            // 
+            // Token_Type
+            // 
+            this.Token_Type.HeaderText = "Token Type";
+            this.Token_Type.MinimumWidth = 6;
+            this.Token_Type.Name = "Token_Type";
+            this.Token_Type.Width = 125;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(71, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.Size = new System.Drawing.Size(137, 35);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "INPUT";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(1040, 545);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dgvTokens);
+            this.Controls.Add(this.btnSplit);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTokens)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSplit;
+        private System.Windows.Forms.DataGridView dgvTokens;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lexeme;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Token_Type;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("project compiler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("project compiler")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3fdb0a9a-6c55-481a-8e78-54257fbb8758")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
